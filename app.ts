dfec130a4fe554d7ab414350f3d78ca16523b690
Manuel Martins <manuel_tarouca@hotmail.com>
2021-06-12 22:10:28 +0100

    Helper function to clean previous submissions

--- app.ts
+++ app.ts
@@ -22,20 +22,42 @@ const streamOpts = {
   pollTime: 5000
 };
 
-const comments =  new CommentStream(client, streamOpts);
+const comments = new CommentStream(client, streamOpts);
 const submissions = new SubmissionStream(client, streamOpts);
 
-submissions.on('item', (submission) => {
-  //console.log(submission);
-  if (submission.is_crosspostable){
-    (<any>submission).submitCrosspost({
-      subredditName: 'censugal',
-      title: submission.title,
-      sendReplies: false,
-      resubmit: false
-    });
-    console.log('Submission by: ' + submission.author.name + ' was crossposted.');
-  } else {
-    console.log('Submission by: ' + submission.author.name + ' was not crossposted.');
-  } 
-});
+function mirror() {
+  submissions.on('item', (submission) => {
+    //console.log(submission);
+    if (submission.is_crosspostable) {
+      (<any>submission).submitCrosspost({
+        subredditName: 'censugal',
+        title: submission.title,
+        sendReplies: false,
+        resubmit: false
+      });
+      console.log('Submission by: ' + submission.author.name + ' was crossposted.');
+    } else {
+      console.log('Submission by: ' + submission.author.name + ' was not crossposted.');
+    }
+  });
+}
+
+function resetMySubmissions() {
+  client.getUser('A_Besuga').getSubmissions({ results: 100 }).then(subs => {
+    let total = subs.length;
+    subs.forEach(sub => {
+      sub.delete().then(() => {
+        total--;
+        console.log('deleted' + total)
+      })
+    })
+  })
+  // submissions.on('item', (submission) => {
+  //   console.log('Removing item from ' + submission.author.name);
+  //   submission.delete();
+
+  // });
+}
+
+const reset = true;
+reset ? resetMySubmissions() : mirror();
